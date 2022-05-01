407e37e94dd4afbd4c4ac77f0a8532ee814d0198
Alexandre <alexandrefsoficial@gmail.com>
2022-05-01 20:21:44 -0300

    controller de users e routes atualizada

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
+      
+      scope 'users' do
+        get 'login', to: 'users#login'
+      end
+
       scope 'categories' do
         get 'index', to: 'categories#index'
         get 'show/:id', to: 'categories#show'
@@ -11,6 +16,7 @@ Rails.application.routes.draw do
         patch 'update/:id', to: 'categories#update'
         delete 'delete/:id', to: 'categories#delete'
       end
+      
       scope 'products' do
         get 'index', to: 'products#index'
         get 'show/:id', to: 'products#show'
@@ -19,6 +25,7 @@ Rails.application.routes.draw do
         patch 'update/:id', to: 'products#update'
         delete 'delete/:id', to: 'products#delete'
       end
+      
     end
   end
 end
